a63fdf9617df0f49d23547666f39a63b79e2bf53
Milonika <fkbyf-2003zv@mail.ru>
2022-02-11 18:54:13 +0300

    Добавьте файлы проекта.

---
+++ AplusApp/AplusApp/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using AplusApp.TabPages;
+namespace AplusApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new InformationProject();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ AplusApp/AplusApp/Db/ProgectModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AplusApp.Db
+{
+    class ProgectModel
+    {
+        public string Name { get; set; }
+        public string Decription { get; set; }
+
+
+    }
+}
